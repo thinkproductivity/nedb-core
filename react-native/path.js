30b8a1fb9dd05918f67480cbce3c7bd043f3cadd
Eric Kryski <e.kryski@gmail.com>
2017-03-25 14:05:07 -0600

    starting to take a crack at react native support

---
+++ react-native/path.js
@@ -0,0 +1,41 @@
+/*
+ * Polyfill for the core Node.js path module
+ */
+
+function assertPath(path) {
+  if (typeof path !== 'string') {
+    throw new TypeError('Path must be a string. Received ' + path);
+  }
+}
+
+module.exports = {
+  // This only supports Unix filepaths which both Android and iOS use
+  // Taken from the core Node.js path module.
+  dirname: function(path) {
+    assertPath(path);
+    if (path.length === 0)
+      return '.';
+    var code = path.charCodeAt(0);
+    var hasRoot = (code === 47/*/*/);
+    var end = -1;
+    var matchedSlash = true;
+    for (var i = path.length - 1; i >= 1; --i) {
+      code = path.charCodeAt(i);
+      if (code === 47/*/*/) {
+        if (!matchedSlash) {
+          end = i;
+          break;
+        }
+      } else {
+        // We saw the first non-path separator
+        matchedSlash = false;
+      }
+    }
+
+    if (end === -1)
+      return hasRoot ? '/' : '.';
+    if (hasRoot && end === 1)
+      return '//';
+    return path.slice(0, end);
+  }
+};
